24300a8040cec8f727594c68651b274a8e4ee95c
Mikhael D'Amato <damato578@gmail.com>
2020-04-08 15:35:21 -0300

    Adicionando Screen -> Game e Menu

---
+++ core/src/com/damato/brothers/stealtheflag/game/screens/GameScreen.java
@@ -0,0 +1,56 @@
+package com.damato.brothers.stealtheflag.game.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen{
+	
+	public GameScreen() {
+	
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		dispose();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+	
+
+}
