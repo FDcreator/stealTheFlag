a4fdda97f99621526fdfefb8c89c3c73d43939ed
Mikhael D'Amato <damato578@gmail.com>
2020-04-11 13:48:45 -0300

    adicionado player in server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
-app = require('express')();
-server = require('http').Server(app);
-io = require('socket.io')(server);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var players = [];
 
 io.on('connection', (socket) => {
-	console.log('User Connected');
+	console.log('User Connected: ' + socket.id);
 	
 	socket.on('disconnect', () => {
-		console.log('User Disconnected');
+		console.log('User Disconnected: ' + socket.id);
 	});
 });
 
 server.listen(3000, () => {
 	console.log('Listening in port=3000')
 });
+
+function Player(id, x, y, stage) {
+	this.id = id;
+	this.x = x;
+	this.y = y;
+	this.stage = stage;
+}
